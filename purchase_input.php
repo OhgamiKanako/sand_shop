847c6ca239dfda8a7aae6886dc9e2ca77d66a68f
OhgamiKanako <o2605.rf8k21d5ptr@gmail.com>
2021-02-11 00:42:33 +0900

    first commit

---
+++ purchase_input.php
@@ -0,0 +1,36 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>購入画面</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<?php require 'menu.php'; ?>
+	<?php
+	 //ログインできてるかどうかの確認(異常処理)
+	 if(!isset($_SESSION['customer'])){
+		 echo '購入手続きを行うにはログインしてください。';
+	//買い物かごが空の場合。(異常処理)
+	 }elseif(empty($_SESSION['product'])){
+		 echo 'カートに商品がありません！';
+	//正常な処理のelse(正常処理)
+	 }else{?>
+	<!-- PHP処理から抜けるので、HTMLでHP部分を構築できる -->
+	<p>お名前：<?= $_SESSION['customer']['name'] ?></p>
+	<p>ご住所：<?= $_SESSION['customer']['address'] ?></p>
+	<hr>
+	<?php require 'cart.php'?>
+	<hr>
+	<p>内容をご確認いただき、購入を確定してください。</p>
+	<a href="purchase_output.php">購入を確定する</a>
+	
+	<?php
+		}
+	?>
+</body>
+
+</html>
---
+++ purchase_output.php
@@ -0,0 +1,52 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>購入画面</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<?php require 'menu.php'; ?>
+	<?php
+	 require 'db_connect.php';
+	 //purchaseテーブル最終行 id+1を取得
+	 $purchase_id = 1;
+	 foreach($pdo->query('select max(id) from purchase') as $row){
+		 $purchase_id = $row['max(id)']+1;
+	 }
+	 //SQL文を作っちゃおう。
+	 $sql = "insert into purchase values(:id,:customer_id)";
+	 //プリペアードステートメントを作る
+	 $stm = $pdo->prepare($sql);
+	 //プリペアードステートメントに値を紐付けする。
+	 $stm->bindValue(':id',$purchase_id, PDO::PARAM_INT);
+	 $stm->bindValue(':customer_id',$_SESSION['customer']['id'], PDO::PARAM_INT);
+	 if($stm->execute()){
+		 //sqlに登録出来た。
+		 //セッションに入っている商品の数だけpurchase_detailに保存しちゃおう。
+		 foreach($_SESSION['product'] as $product_id => $product){
+			 //sql文を作る（プレースホルダーを使った式）
+			 $sql = "insert into purchase_detail values(:purchase_id,:product_id,:count)";
+			 //プリペアードステートメント作成。
+			 $stm = $pdo->prepare($sql);
+			 //プリペアードステートメントに値をバインドする
+			 $stm->bindValue(':purchase_id',$purchase_id, PDO::PARAM_INT);
+			 $stm->bindValue(':product_id',$product_id, PDO::PARAM_INT);
+			 $stm->bindValue(':count',$product['count'], PDO::PARAM_INT);
+			 //SQL文を実行する。
+			 $stm->execute();
+		 }
+		 //買い物が終わったら、カートの中身を消さなければ。
+		 unset($_SESSION['product']);
+		 echo "購入手続きが完了いたしました。ありがとうございます。";
+	 }else{
+		 //sqlに登録できなかった。
+		 echo "購入手続き中にエラーが発生いたしました。申し訳ございません。";
+	 }
+	?>
+</body>
+
+</html>
